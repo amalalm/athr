229e2fa84026e00634f8a0565af643edc096a0c0
amalalm <amalmoh.mtr@gmail.com>
2024-12-16 16:54:54 +0300

    Initial Commit

---
+++ athr/athrApp.swift
@@ -0,0 +1,17 @@
+//
+//  athrApp.swift
+//  athr
+//
+//  Created by Amal almutairi on 15/06/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct athrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
